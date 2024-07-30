3e6c973339c4bec337c7e23e5f80d744c2f358dd
MThelic <47725739+MThelic@users.noreply.github.com>
2024-07-30 11:08:21 -0300

    Update listar_codigos.py

--- pages/listar_codigos.py
+++ pages/listar_codigos.py
@@ -12,9 +12,11 @@ for root, _, files in os.walk('.'):
         if file.endswith('.py'):
             arquivos[file] = os.path.join(root, file)
 
-arquivo_selecionado = st.selectbox("Selecione um arquivo:", list(arquivos.keys()))
+cols=st.columns([3,1])
+arquivo_selecionado = cols[0].selectbox("Selecione um arquivo:", list(arquivos.keys()))
 
 if arquivo_selecionado:
     with open(arquivos[arquivo_selecionado], "r", encoding='utf-8') as f:
         codigo = f.read()
     st.code(codigo, language="python")
+    cols[1].download_button("Baixar código", codigo, file_name=arquivo_selecionado)
